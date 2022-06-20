bc7e6dd5bd75b04a3f082f5ab275549a9383e4c8
Waleed Chaudhry <54864665+wachaudh@users.noreply.github.com>
2022-06-19 23:29:57 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = process.env.HOME;
+const port = process.env.SERVICE_PORT;
 
 app.get("/", (req, res) => {
   console.log(`${new Date()} ${req.method} ${req.path}`);
