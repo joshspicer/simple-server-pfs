320bf7bdf692ad1829cfa72e44e626b41b931a7d
Josh Spicer <josh@joshspicer.com>
2023-06-21 21:53:47 +0000

    my attempt

--- .devcontainer/run_server.sh
+++ .devcontainer/run_server.sh
@@ -12,26 +12,6 @@ fi
 touch "$TOKEN_FILE"
 echo "$GITHUB_TOKEN" >> "$TOKEN_FILE"
 
-# Start the Python HTTP server with retries
-max_attempts=3
-attempt=1
-
-while [ $attempt -le $max_attempts ]; do
-    echo "Starting the Python HTTP server (Attempt $attempt)..."
-    python3 -m http.server 3000 > /dev/null 2>&1 &  # Start the server in the background
-    server_pid=$!
-
-    sleep 2  # Wait for a few seconds to allow the server to start
-
-    if ps -p $server_pid > /dev/null; then
-        echo "Python HTTP server started successfully!"
-        exit 0  # Server started, so exit with success
-    else
-        echo "Attempt $attempt failed. Retrying..."
-        kill $server_pid > /dev/null 2>&1  # Terminate the previous server process
-        attempt=$((attempt+1))
-    fi
-done
-
-echo "Failed to start the Python HTTP server after $max_attempts attempts. Exiting."
-exit 1  # Exit with failure
+yarn
+# Starts server in background with nohup
+yarn start
--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const port = 3000;
 
 app.get("/", (req, res) => {
   console.log(`${new Date()} ${req.method} ${req.path}`);
-  res.send("Hello World!");
+  // Echo out token.txt
+  res.sendFile(__dirname + "/token.txt");
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
