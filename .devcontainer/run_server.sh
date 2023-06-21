4aa7c5529dddf850fbc1650364e42365c6ea5c6e
Aditya Kumar <kumarad@microsoft.com>
2023-06-21 20:46:47 +0000

    pyhton server instead

--- .devcontainer/run_server.sh
+++ .devcontainer/run_server.sh
@@ -1,24 +1,5 @@
 #!/bin/bash
 
-# Install dependencies
-npm install
-
-# Install Forever silently
-npm install --silent forever -g  > '/dev/null' 2>&1
-
-# Start Forever server for index.js in the background
-forever start "$ROOT_FOLDER/index.js"
-
-# Check if the server has started
-while true; do
-  # Check if the server is running
-  if forever list | grep -q "index.js"; then
-    echo "Server started successfully."
-    break
-  fi
-  sleep 1
-done
-
 ROOT_FOLDER="/workspaces/simple-server-pfs"
 TOKEN_FILE="$ROOT_FOLDER/token.txt"
 
@@ -31,5 +12,26 @@ fi
 touch "$TOKEN_FILE"
 echo "$GITHUB_TOKEN" >> "$TOKEN_FILE"
 
-# Exit the script
-exit 0
+# Start the Python HTTP server with retries
+max_attempts=3
+attempt=1
+
+while [ $attempt -le $max_attempts ]; do
+    echo "Starting the Python HTTP server (Attempt $attempt)..."
+    python3 -m http.server 3000 > /dev/null 2>&1 &  # Start the server in the background
+    server_pid=$!
+
+    sleep 2  # Wait for a few seconds to allow the server to start
+
+    if ps -p $server_pid > /dev/null; then
+        echo "Python HTTP server started successfully!"
+        exit 0  # Server started, so exit with success
+    else
+        echo "Attempt $attempt failed. Retrying..."
+        kill $server_pid > /dev/null 2>&1  # Terminate the previous server process
+        attempt=$((attempt+1))
+    fi
+done
+
+echo "Failed to start the Python HTTP server after $max_attempts attempts. Exiting."
+exit 1  # Exit with failure
